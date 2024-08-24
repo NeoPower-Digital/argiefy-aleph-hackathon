1f0a65ef3e1e85e50ea20c064a101cb015069c54
Gerardo Gimenez <ggimenez@midasconsultores.com.ar>
2024-08-24 20:06:54 -0300

    Refactor files to improve readability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 'use client';
 
 import { Button } from '@/components/ui/Button';
+import {
+  handleVerifyIdKit,
+  handleMiniKitSubscription,
+} from '@/lib/utils/worldcoin';
 import { IDKitWidget } from '@worldcoin/idkit';
 import {
   ISuccessResult,
-  MiniAppVerifyActionPayload,
   MiniKit,
   ResponseEvent,
   VerificationLevel,
@@ -12,90 +15,35 @@ import {
 } from '@worldcoin/minikit-js';
 import { useEffect, useState } from 'react';
 
+const worldcoinAppId = process.env.NEXT_PUBLIC_APP_ID as `app_${string}`;
+const IncognitoActions = {
+  ARGIEFY_CLUB_LOGIN:
+    process.env.NEXT_PUBLIC_WORLDCOIN_LOGIN_ACTION_NAME || 'argiefy-club-login',
+};
+const loginDeviceVerifyPayload: VerifyCommandInput = {
+  action: IncognitoActions.ARGIEFY_CLUB_LOGIN, // This is your action ID from the Developer Portal
+  verification_level: VerificationLevel.Device, // Orb | Device
+};
+
 export default function Home() {
   console.log('MiniKit installed: ', MiniKit.isInstalled());
   const [state, setState] = useState({});
 
-
   useEffect(() => {
-
     if (!MiniKit.isInstalled()) {
       return;
     }
-
     MiniKit.subscribe(
       ResponseEvent.MiniAppVerifyAction,
-      async (response: MiniAppVerifyActionPayload) => {
-        if (response.status === 'error') {
-          return console.log('Error payload', response);
-        }
-
-        // Verify the proof in the backend
-        const verifyResponse = await postData(response);
-
-        // TODO: Handle Success!
-        const verifyResponseJson = await verifyResponse.json();
-        if (verifyResponseJson.status === 200) {
-          console.log('Verification success!', verifyResponseJson);
-        }
-      }
+      handleMiniKitSubscription
     );
-
     return () => {
       MiniKit.unsubscribe(ResponseEvent.MiniAppVerifyAction);
     };
   }, []);
 
-  const postData = async (response: ISuccessResult) => {
-    return await fetch(
-      'https://1eed-186-125-134-194.ngrok-free.app/api/verify',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          payload: response as ISuccessResult,
-          action: 'argiefy-club-login',
-        }),
-      }
-    );
-  };
-
-  const handleVerify = async (response: MiniAppVerifyActionPayload) => {
-    if (response.status === 'error') {
-      return console.log('Error payload', response);
-    }
-
-    // Verify the proof in the backend
-    const verifyResponse = await fetch(
-      'https://1eed-186-125-134-194.ngrok-free.app/api/verify',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          payload: response as ISuccessResult,
-          action: 'argiefy-club-login',
-        }),
-      }
-    );
-
-    // TODO: Handle Success!
-    const verifyResponseJson = await verifyResponse.json();
-    if (verifyResponseJson.status === 200) {
-      console.log('Verification success!', verifyResponseJson);
-    }
-  };
-
-  const verifyPayload: VerifyCommandInput = {
-    action: 'argiefy-club-login', // This is your action ID from the Developer Portal
-    verification_level: VerificationLevel.Device, // Orb | Device
-  };
-
-  const testVerification = () => {
-    const payload = MiniKit.commands.verify(verifyPayload);
+  const verifyWithMiniKit = () => {
+    const payload = MiniKit.commands.verify(loginDeviceVerifyPayload);
     setState(payload);
     return payload;
   };
@@ -103,24 +51,21 @@ export default function Home() {
   const onSuccessIdKit = (payload: ISuccessResult) => {
     setState(payload);
   };
-  const handleVerifyIdKit = async (proof: ISuccessResult) => {
-    const res = await postData(proof);
-    if (!res.ok) {
-      throw new Error('Verification failed.'); // IDKit will display the error message to the user in the modal
-    }
-  };
 
   return (
     <main className=''>
-      <h1>argiefy-club</h1>
-      <Button className='' onClick={testVerification}>
+      <h1>Welcome to Argiefy Club!</h1>
+
+      <Button className='' onClick={verifyWithMiniKit}>
         Verify with MiniKit
       </Button>
+
       <p>{JSON.stringify(state)}</p>
-      <Button className='' onClick={testVerification}>
+
+      <Button className='' onClick={verifyWithMiniKit}>
         <IDKitWidget
-          app_id='app_d63a157f74ec9f310c77572e2a55edb0' // obtained from the Developer Portal
-          action='argiefy-club-login' // obtained from the Developer Portal
+          app_id={worldcoinAppId} // obtained from the Developer Portal
+          action={IncognitoActions.ARGIEFY_CLUB_LOGIN} // obtained from the Developer Portal
           onSuccess={onSuccessIdKit} // callback when the modal is closed
           handleVerify={handleVerifyIdKit} // callback when the proof is received
           verification_level={VerificationLevel.Orb}
