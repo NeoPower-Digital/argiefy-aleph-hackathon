e9aaadb63bdc75c6c700c6b7e0e92e175ce93c08
Gerardo Gimenez <ggimenez@midasconsultores.com.ar>
2024-08-24 17:40:41 -0300

    Add World ID verification

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,86 +1,77 @@
 'use client';
 
-import { Button } from "@/components/ui/Button";
-import { IDKitWidget } from "@worldcoin/idkit";
-import { MiniKit, ResponseEvent, VerificationLevel, VerifyCommandInput } from "@worldcoin/minikit-js";
-import { useEffect } from "react";
-
+import { Button } from '@/components/ui/Button';
+import {
+  ISuccessResult,
+  MiniAppVerifyActionPayload,
+  MiniKit,
+  ResponseEvent,
+  VerificationLevel,
+  VerifyCommandInput,
+} from '@worldcoin/minikit-js';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
-
-  const verifyPayload: VerifyCommandInput = {
-    action: "profile-human-validation",
-    verification_level: VerificationLevel.Device,
-  };
-
-
-  const payload = MiniKit.commands.verify(verifyPayload);
-
-  console.log(payload);
-
+  console.log('MiniKit installed: ', MiniKit.isInstalled());
+  const [state, setState] = useState({});
   useEffect(() => {
     if (!MiniKit.isInstalled()) {
       return;
     }
 
-    MiniKit.subscribe(ResponseEvent.MiniAppVerifyAction, async (response) => {
-      //   if (payload.status === "error") {
-      //   return console.log("Error payload", payload);
-      // }
+    MiniKit.subscribe(
+      ResponseEvent.MiniAppVerifyAction,
+      async (response: MiniAppVerifyActionPayload) => {
+        if (response.status === 'error') {
+          return console.log('Error payload', response);
+        }
 
+        // Verify the proof in the backend
+        const verifyResponse = await fetch(
+          'https://1eed-186-125-134-194.ngrok-free.app/api/verify',
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              payload: response as ISuccessResult,
+              action: 'argiefy-club-login',
+            }),
+          }
+        );
 
-      // Verify the proof in the backend 
-      const verifyResponse = await fetch("https://1eed-186-125-134-194.ngrok-free.app/api/verify", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          payload: response,
-        })
-
-      });
-      console.log(verifyResponse);
-    });
+        // TODO: Handle Success!
+        const verifyResponseJson = await verifyResponse.json();
+        if (verifyResponseJson.status === 200) {
+          console.log('Verification success!', verifyResponseJson);
+        }
+      }
+    );
 
     return () => {
-      // Clean up on unmount
       MiniKit.unsubscribe(ResponseEvent.MiniAppVerifyAction);
     };
   }, []);
 
-  // const onSuccess = () => { }
-  // const handleVerify = () => { }
+  const verifyPayload: VerifyCommandInput = {
+    action: 'argiefy-club-login', // This is your action ID from the Developer Portal
+    verification_level: VerificationLevel.Device, // Orb | Device
+  };
 
-  const postData = async () => {
-    const response = await fetch('https://1eed-186-125-134-194.ngrok-free.app/api/verify', {
-      method: 'POST',
-      // mode: 'no-cors',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ payload: payload }),
-    });
-    console.log(response)
-  }
+  const testVerification = () => {
+    const payload = MiniKit.commands.verify(verifyPayload);
+    setState(payload);
+    return payload;
+  };
 
   return (
-    <main className="">
+    <main className=''>
       <h1>argiefy-club</h1>
-      {/* <IDKitWidget
-        app_id="app_39c0671f96ddf3ab0fd057fba96180ec" // obtained from the Developer Portal
-        action="profile-human-validation" // obtained from the Developer Portal
-        onSuccess={onSuccess} // callback when the modal is closed
-        handleVerify={handleVerify} // callback when the proof is received
-        verification_level={VerificationLevel.Device}
-      >
-        {({ open }) =>
-          // This is the button that will open the IDKit modal
-          <button onClick={open}>Verify with World ID</button>
-        }
-      </IDKitWidget> */}
-
-      <Button className="" onClick={postData}>hi</Button>
+      <Button className='' onClick={testVerification}>
+        Verify World ID
+      </Button>
+      <p>{JSON.stringify(state)}</p>
     </main>
   );
 }
