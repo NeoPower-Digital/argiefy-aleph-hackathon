abb313b9c384ea5829ea621f5429fe2d232d14c0
Gerardo Gimenez <ggimenez@midasconsultores.com.ar>
2024-08-24 18:11:07 -0300

    Add verification with IDKit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Button } from '@/components/ui/Button';
+import { IDKitWidget } from '@worldcoin/idkit';
 import {
   ISuccessResult,
   MiniAppVerifyActionPayload,
@@ -27,19 +28,7 @@ export default function Home() {
         }
 
         // Verify the proof in the backend
-        const verifyResponse = await fetch(
-          'https://1eed-186-125-134-194.ngrok-free.app/api/verify',
-          {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-              payload: response as ISuccessResult,
-              action: 'argiefy-club-login',
-            }),
-          }
-        );
+        const verifyResponse = await postData(response);
 
         // TODO: Handle Success!
         const verifyResponseJson = await verifyResponse.json();
@@ -54,6 +43,49 @@ export default function Home() {
     };
   }, []);
 
+  const postData = async (response: ISuccessResult) => {
+    return await fetch(
+      'https://1eed-186-125-134-194.ngrok-free.app/api/verify',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          payload: response as ISuccessResult,
+          action: 'argiefy-club-login',
+        }),
+      }
+    );
+  };
+
+  const handleVerify = async (response: MiniAppVerifyActionPayload) => {
+    if (response.status === 'error') {
+      return console.log('Error payload', response);
+    }
+
+    // Verify the proof in the backend
+    const verifyResponse = await fetch(
+      'https://1eed-186-125-134-194.ngrok-free.app/api/verify',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          payload: response as ISuccessResult,
+          action: 'argiefy-club-login',
+        }),
+      }
+    );
+
+    // TODO: Handle Success!
+    const verifyResponseJson = await verifyResponse.json();
+    if (verifyResponseJson.status === 200) {
+      console.log('Verification success!', verifyResponseJson);
+    }
+  };
+
   const verifyPayload: VerifyCommandInput = {
     action: 'argiefy-club-login', // This is your action ID from the Developer Portal
     verification_level: VerificationLevel.Device, // Orb | Device
@@ -65,13 +97,37 @@ export default function Home() {
     return payload;
   };
 
+  const onSuccessIdKit = (payload: ISuccessResult) => {
+    setState(payload);
+  };
+  const handleVerifyIdKit = async (proof: ISuccessResult) => {
+    const res = await postData(proof);
+    if (!res.ok) {
+      throw new Error('Verification failed.'); // IDKit will display the error message to the user in the modal
+    }
+  };
+
   return (
     <main className=''>
       <h1>argiefy-club</h1>
       <Button className='' onClick={testVerification}>
-        Verify World ID
+        Verify with MiniKit
       </Button>
       <p>{JSON.stringify(state)}</p>
+      <Button className='' onClick={testVerification}>
+        <IDKitWidget
+          app_id='app_d63a157f74ec9f310c77572e2a55edb0' // obtained from the Developer Portal
+          action='argiefy-club-login' // obtained from the Developer Portal
+          onSuccess={onSuccessIdKit} // callback when the modal is closed
+          handleVerify={handleVerifyIdKit} // callback when the proof is received
+          verification_level={VerificationLevel.Orb}
+        >
+          {({ open }) => (
+            // This is the button that will open the IDKit modal
+            <button onClick={open}>Verify with IDKit</button>
+          )}
+        </IDKitWidget>
+      </Button>
     </main>
   );
 }
