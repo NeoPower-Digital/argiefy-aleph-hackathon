2a502b96a837e082cc9af9e1fdc0272c4ebf918e
pcorrado-np <pcorrado@neopower.digital>
2024-08-24 17:08:40 -0300

    verify with button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,86 +1,60 @@
-'use client';
+"use client";
 
 import { Button } from "@/components/ui/Button";
 import { IDKitWidget } from "@worldcoin/idkit";
-import { MiniKit, ResponseEvent, VerificationLevel, VerifyCommandInput } from "@worldcoin/minikit-js";
+import {
+  MiniKit,
+  ResponseEvent,
+  VerificationLevel,
+  VerifyCommandInput,
+} from "@worldcoin/minikit-js";
 import { useEffect } from "react";
 
-
 export default function Home() {
+  const verify = () => {
+    const verifyPayload: VerifyCommandInput = {
+      action: "profile-human-validation",
+      verification_level: VerificationLevel.Device,
+    };
 
-  const verifyPayload: VerifyCommandInput = {
-    action: "profile-human-validation",
-    verification_level: VerificationLevel.Device,
+    MiniKit.commands.verify(verifyPayload);
   };
 
+  // useEffect(() => {
+  //   if (!MiniKit.isInstalled()) {
+  //     return;
+  //   }
 
-  const payload = MiniKit.commands.verify(verifyPayload);
+  //   MiniKit.subscribe(ResponseEvent.MiniAppVerifyAction, async (response) => {
+  //     if (response.status === "error") {
+  //       return console.log("Error payload", response);
+  //     }
 
-  console.log(payload);
+  //     // Verify the proof in the API
+  //     await fetch("/api/verify", {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify({
+  //         payload: response,
+  //       }),
+  //     });
+  //   });
 
-  useEffect(() => {
-    if (!MiniKit.isInstalled()) {
-      return;
-    }
-
-    MiniKit.subscribe(ResponseEvent.MiniAppVerifyAction, async (response) => {
-      //   if (payload.status === "error") {
-      //   return console.log("Error payload", payload);
-      // }
-
-
-      // Verify the proof in the backend 
-      const verifyResponse = await fetch("https://1eed-186-125-134-194.ngrok-free.app/api/verify", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          payload: response,
-        })
-
-      });
-      console.log(verifyResponse);
-    });
-
-    return () => {
-      // Clean up on unmount
-      MiniKit.unsubscribe(ResponseEvent.MiniAppVerifyAction);
-    };
-  }, []);
-
-  // const onSuccess = () => { }
-  // const handleVerify = () => { }
-
-  const postData = async () => {
-    const response = await fetch('https://1eed-186-125-134-194.ngrok-free.app/api/verify', {
-      method: 'POST',
-      // mode: 'no-cors',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ payload: payload }),
-    });
-    console.log(response)
-  }
+  //   return () => {
+  //     // Clean up on unmount
+  //     MiniKit.unsubscribe(ResponseEvent.MiniAppVerifyAction);
+  //   };
+  // }, []);
 
   return (
-    <main className="">
+    <main>
       <h1>argiefy-club</h1>
-      {/* <IDKitWidget
-        app_id="app_39c0671f96ddf3ab0fd057fba96180ec" // obtained from the Developer Portal
-        action="profile-human-validation" // obtained from the Developer Portal
-        onSuccess={onSuccess} // callback when the modal is closed
-        handleVerify={handleVerify} // callback when the proof is received
-        verification_level={VerificationLevel.Device}
-      >
-        {({ open }) =>
-          // This is the button that will open the IDKit modal
-          <button onClick={open}>Verify with World ID</button>
-        }
-      </IDKitWidget> */}
 
-      <Button className="" onClick={postData}>hi</Button>
+      <Button className="" onClick={verify}>
+        Verify
+      </Button>
     </main>
   );
 }
