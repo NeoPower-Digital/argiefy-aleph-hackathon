2fbe757c790cedfe2ea8238656c5ba3ae53d48a5
juanarancibia <arancibia.juan22@gmail.com>
2024-08-25 02:04:08 -0300

    refactor buttonClaim

--- src/components/ButtonClaim.tsx
+++ src/components/ButtonClaim.tsx
@@ -33,61 +33,56 @@ const ButtonClaim: FC<ButtonClaimProps> = ({
   isMiniKit,
   onSuccessIdKit,
 }) => {
-  {
-    if (!isMiniKit)
-      return isVerified ? (
-        <MatecitoDialog />
-      ) : (
-        <IDKitWidget
-          app_id={worldcoinAppId} // obtained from the Developer Portal
-          action={IncognitoActions.ARGIEFY_CLUB_LOGIN} // obtained from the Developer Portal
-          onSuccess={onSuccessIdKit} // callback when the modal is closed
-          handleVerify={handleVerifyIdKit} // callback when the proof is received
-          verification_level={VerificationLevel.Device}
-        >
-          {({ open }) => (
-            <>
-              <Button
-                className="w-full max-w-md mt-4 p-4 bg-gray-900 hover:bg-gray-800 text-white rounded-lg flex items-center justify-center transition-colors"
-                onClick={open}
-              >
-                <div className="flex items-center justify-center space-x-2">
-                  <Image
-                    width={20}
-                    height={20}
-                    src={FingerPrint}
-                    alt="Fingerprint Icon"
-                  />
-                  <div className="text-lg font-medium">Verify Identity</div>
-                </div>
-              </Button>
-            </>
-          )}
-        </IDKitWidget>
-      );
+  if (isVerified) {
+    return <MatecitoDialog />;
+  }
+
+  if (isMiniKit) {
+    return (
+      <Button
+        className="w-full max-w-md mt-4 p-4 bg-gray-900 hover:bg-gray-800 text-white rounded-lg flex items-center justify-center transition-colors"
+        onClick={handleClick}
+      >
+        <div className="flex items-center justify-center space-x-2">
+          <Image
+            width={20}
+            height={20}
+            src={FingerPrint}
+            alt="Fingerprint Icon"
+          />
+          <div className="text-lg font-medium">Verify Identity</div>
+        </div>
+      </Button>
+    );
   }
 
   return (
-    <>
-      {isVerified ? (
-        <MatecitoDialog />
-      ) : (
-        <Button
-          className="w-full max-w-md mt-4 p-4 bg-gray-900 hover:bg-gray-800 text-white rounded-lg flex items-center justify-center transition-colors"
-          onClick={handleClick}
-        >
-          <div className="flex items-center justify-center space-x-2">
-            <Image
-              width={20}
-              height={20}
-              src={FingerPrint}
-              alt="Fingerprint Icon"
-            />
-            <div className="text-lg font-medium">Verify Identity</div>
-          </div>
-        </Button>
+    <IDKitWidget
+      app_id={worldcoinAppId} // obtained from the Developer Portal
+      action={IncognitoActions.ARGIEFY_CLUB_LOGIN} // obtained from the Developer Portal
+      onSuccess={onSuccessIdKit} // callback when the modal is closed
+      handleVerify={handleVerifyIdKit} // callback when the proof is received
+      verification_level={VerificationLevel.Device}
+    >
+      {({ open }) => (
+        <>
+          <Button
+            className="w-full max-w-md mt-4 p-4 bg-gray-900 hover:bg-gray-800 text-white rounded-lg flex items-center justify-center transition-colors"
+            onClick={open}
+          >
+            <div className="flex items-center justify-center space-x-2">
+              <Image
+                width={20}
+                height={20}
+                src={FingerPrint}
+                alt="Fingerprint Icon"
+              />
+              <div className="text-lg font-medium">Verify Identity</div>
+            </div>
+          </Button>
+        </>
       )}
-    </>
+    </IDKitWidget>
   );
 };
 
